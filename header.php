07d5779b0c7f7d18788d4c8a3d3b88a688e0a036
leonidlezner <kontakt@lezner.de>
2018-03-03 12:01:20 +0100

    Changing the header search

--- header.php
+++ header.php
@@ -25,9 +25,13 @@
           <div class="collapse navbar-collapse" id="navbarNav">
 		  	<?php bootstrap_navigation('header-menu', 'navbar-nav mr-auto mt-2 mt-lg-0'); ?>
 
-            <form class="form-inline my-2 my-lg-0" action="<?php echo home_url( '/' ); ?>" method="get">
-               <input class="form-control mr-sm-2" name="s" value="<?php the_search_query(); ?>" type="search" placeholder="<?php _e('Search', 'sendeturm'); ?>" aria-label="<?php _e('Search', 'sendeturm'); ?>">
-               <button class="btn my-2 my-sm-0" type="submit"><?php _e('Go', 'sendeturm'); ?></button>
+            <form id="header-search" class="form-inline " action="<?php echo home_url( '/' ); ?>" method="get">
+               	<input class="form-control" name="s" value="<?php the_search_query(); ?>" type="search" placeholder="<?php _e('Search', 'sendeturm'); ?>" aria-label="<?php _e('Search', 'sendeturm'); ?>">
+               	<button class="btn my-2 my-sm-0" type="submit">
+			   	<svg width="15px" height="15px">
+                    <path d="M11.618 9.897l4.224 4.212c.092.09.1.23.02.312l-1.464 1.46c-.08.08-.222.072-.314-.02L9.868 11.66M6.486 10.9c-2.42 0-4.38-1.955-4.38-4.367 0-2.413 1.96-4.37 4.38-4.37s4.38 1.957 4.38 4.37c0 2.412-1.96 4.368-4.38 4.368m0-10.834C2.904.066 0 2.96 0 6.533 0 10.105 2.904 13 6.486 13s6.487-2.895 6.487-6.467c0-3.572-2.905-6.467-6.487-6.467 "></path>
+				</svg>
+				</button>
              </form>
           </div>
         </div>
