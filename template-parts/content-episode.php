7b29876f6dfe88ea303cb5d9cc8aaabd217f5cda
leonidlezner <kontakt@lezner.de>
2018-03-13 20:43:34 +0100

    Changing the related episodes widget

--- template-parts/content-episode.php
+++ template-parts/content-episode.php
@@ -69,7 +69,7 @@ $episode = \Podlove\get_episode();
                 <li class="list-group-item contributor">
                     <div class="row no-gutters">
                         <div class="contributor-pic col mb-sm-2">
-                            <img src="<?php echo $contributor->image()->url(array('width' => 60, 'height' => 60)); ?>" />
+                            <img src="<?php echo $contributor->image()->url(array('width' => 50, 'height' => 50)); ?>" />
                         </div>
                         <div class="contributor-name col-8 col-sm-8 col-md-12 col-lg-5 col-xl-6">
                             <h4 class="name"><?php echo $contributor->name(); ?></h4>
@@ -80,7 +80,7 @@ $episode = \Podlove\get_episode();
                                 <?php foreach($contributor->services() as $service): ?>
                                 <li class="nav-item mb-1 mr-1">
                                     <a href="<?php echo $service->profileUrl(); ?>">
-                                        <img src="<?php echo $service->image()->url(array('width' => 25, 'height' => 25)); ?>" alt="<?php echo $service->title(); ?>" />
+                                        <img src="<?php echo $service->image()->url(array('width' => 20, 'height' => 20)); ?>" alt="<?php echo $service->title(); ?>" />
                                     </a>
                                 </li>
                                 <?php endforeach; ?>
@@ -103,9 +103,8 @@ $episode = \Podlove\get_episode();
             <?php foreach($episode->relatedEpisodes() as $related): ?>
                 <a href="<?php echo $related->url(); ?>" class="list-group-item list-group-item-action flex-column align-items-start">
                     <div class="d-flex w-100 justify-content-between">
-                    <h5 class="mb-1"><?php echo $related->title(); ?></h5>
+                        <?php echo $related->title(); ?>
                     </div>
-                    <p class="mb-1"><?php echo $related->subTitle(); ?></p>
                 </a>
             <?php endforeach; ?>
             </div>
