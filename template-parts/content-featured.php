070e23f1729924a6dce139022f7e66b8edb9c971
leonidlezner <kontakt@lezner.de>
2018-03-14 22:45:07 +0100

    Fixing issue #1

--- template-parts/content-featured.php
+++ template-parts/content-featured.php
@@ -20,19 +20,24 @@ $feed = \Podlove\Model\Feed::find_one_by_slug('mp3');
         <div class="container">
             <div class="row">
                 <div class="col">
-                    <h1 class="entry-title mb-4 text-center text-sm-left">
-                        <a href="<?php echo esc_url(get_permalink($recent_post[0])); ?>" rel="bookmark"><?php echo $episode->title(); ?></a>
-                        <span class="badge badge-pill badge-success"><?php echo __('New', 'sendeturm'); ?></span>
-                    </h1>
+                    
                 </div>
             </div>
 
             <div class="row">
-                <div class="col-12 col-lg-8">
+                <div class="col-12 col-lg-8 align-self-end">
+                    <h1 class="entry-title mb-4 text-center text-sm-left">
+                        <a href="<?php echo esc_url(get_permalink($recent_post[0])); ?>" rel="bookmark"><?php echo $episode->title(); ?></a>
+                        <span class="badge badge-pill badge-success"><?php echo __('New', 'sendeturm'); ?></span>
+                    </h1>
                     <?php echo $episode->player(); ?>
                 </div>
 
-                <div class="col-lg-4 align-self-end text-xl-right text-center subscribe">
+                <div class="col-lg-4 align-self-end text-center text-lg-right subscribe">                				
+                    <div class="about-info mb-0 mb-lg-2 text-secondary h3">
+                        <?php bloginfo('description'); ?>
+                    </div>
+                    
                     <div class="inner pt-2 pt-lg-0">
                         <a class="btn btn-secondary mb-2" role="button" href="https://itunes.apple.com/de/podcast/id<?php echo $feed->itunes_feed_id; ?>"><?php echo _e('Subscribe with iTunes', 'sendeturm'); ?></a>
                         <a class="btn btn-secondary mb-2" role="button" href="<?php echo $feed->get_subscribe_url(); ?>"><?php echo _e('Subscribe with RSS', 'sendeturm'); ?></a>
