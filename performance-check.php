170f4387cefa99548f540836409277b3633028ea
leonidlezner <kontakt@lezner.de>
2018-03-31 13:27:27 +0200

    Adding missed translation

---
+++ performance-check.php
@@ -0,0 +1,65 @@
+<?php /* Template Name: PerformanceCheck */
+
+$starting_time = microtime(1);
+
+get_header();
+
+$args = array('post_type' => 'podcast', 'posts_per_page' => -1);
+
+$the_query = new WP_Query( $args );
+
+?>
+
+	<?php get_template_part('template-parts/content', 'featured'); ?>
+    
+    <h1 class="text-center mt-4 text-danger">Performance Check</h1>
+
+    <div id="main-content" class="container">
+        <main id="main">
+            <?php get_template_part('template-parts/partial', 'socialbar'); ?>
+
+            <div class="row">
+                <div class="col-12">
+                    <h2><?php echo __('All episodes', 'sendeturm'); ?></h2>
+                    <div id="all-episodes" class="list-group">
+
+                        <?php
+                        if ($the_query->have_posts()) :
+                            
+                            while ($the_query->have_posts())
+                            {
+                                $the_query->the_post();
+
+                                get_template_part('template-parts/content', 'home');
+                            }
+                        ?>
+                        </div><!-- end of list-group -->
+                        <?php the_posts_navigation(array(
+                            'mid_size' => 2
+                        ));
+                        ?>
+                    </div><!-- end of col-X -->
+
+                </div>
+            </div>
+        <?php
+
+            
+        else :
+            get_template_part('template-parts/content', 'none');
+        endif;
+
+        wp_reset_postdata();
+
+        ?>
+        </main>
+    </div>
+
+
+
+</div>
+
+<p class="text-center"><?php echo sprintf('Time to load: %s', microtime(1) - $starting_time); ?></p>
+
+</body>
+</html>
