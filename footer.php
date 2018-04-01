68bc618794f759dacce859e6be38a4d7b2820f53
leonidlezner <kontakt@lezner.de>
2018-04-02 00:38:50 +0200

    Fixing the footer background

--- footer.php
+++ footer.php
@@ -1,39 +1,41 @@
-    </div>
+        </div>
 
-    <footer id="page-footer">
-        <div class="overlay">
-            <div class="container">
-                <div class="row">
-                    <div class="col-6">
-                        <h2><i class="fa fa-microphone mr-1"></i><?php echo __('About', 'sendeturm'); ?> <?php bloginfo('name'); ?></h2>
-                        <p><?php bloginfo('description'); ?></p>
-                    </div>
-                    
-                    <div class="col">
-                        <h2><i class="fa fa-link mr-1"></i><?php echo __('Quick links', 'sendeturm'); ?></h2>
-                        <?php bootstrap_navigation('footer-menu', 'simple-list simple-menu'); ?>
-                    </div>
-                    
-                    <div class="col social-icons">
-                        <h2><i class="fa fa-comment mr-1"></i> <?php echo __('Contact', 'sendeturm'); ?></h2>
-                        <div class="d-flex flex-row h1">
-                        <?php
-                        $podcast = \Podlove\get_podcast();
-                        $services = $podcast->services(array('category' => 'social'));
+        <footer id="page-footer">
+            <div class="overlay">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-6">
+                            <h2><i class="fa fa-microphone mr-1"></i><?php echo __('About', 'sendeturm'); ?> <?php bloginfo('name'); ?></h2>
+                            <p><?php bloginfo('description'); ?></p>
+                        </div>
+                        
+                        <div class="col">
+                            <h2><i class="fa fa-link mr-1"></i><?php echo __('Quick links', 'sendeturm'); ?></h2>
+                            <?php bootstrap_navigation('footer-menu', 'simple-list simple-menu'); ?>
+                        </div>
+                        
+                        <div class="col social-icons">
+                            <h2><i class="fa fa-comment mr-1"></i> <?php echo __('Contact', 'sendeturm'); ?></h2>
+                            <div class="d-flex flex-row h1">
+                            <?php
+                            $podcast = \Podlove\get_podcast();
+                            $services = $podcast->services(array('category' => 'social'));
 
-                        foreach($services as $service): ?>                     
-                            <a href="<?php echo $service->profileUrl(); ?>"><i class="fa fa-<?php echo strtolower($service->title()); ?>-square mr-1"></i></a>
-                        <?php endforeach; ?>
+                            foreach($services as $service): ?>                     
+                                <a href="<?php echo $service->profileUrl(); ?>"><i class="fa fa-<?php echo strtolower($service->title()); ?>-square mr-1"></i></a>
+                            <?php endforeach; ?>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </footer>
+        </footer>
 
-<?php wp_footer(); ?>
+    <?php wp_footer(); ?>
 
-<?php echo get_global_setting('analytics'); ?>
+    <?php echo get_global_setting('analytics'); ?>
+
+</div> <!-- end of page-inner -->
 
 </body>
 </html>
--- header.php
+++ header.php
@@ -13,32 +13,34 @@
 </head>
 
 <body <?php body_class();?>>
-	<a class="skip-link screen-reader-text sr-only sr-only-focusable" href="#content"><?php esc_html_e('Skip to content', 'sendeturm');?></a>
+	<div id="page-inner">
+		
+		<a class="skip-link screen-reader-text sr-only sr-only-focusable" href="#content"><?php esc_html_e('Skip to content', 'sendeturm');?></a>
 
-	<header id="page-header">
-		<div id="main-nav" class="navbar navbar-expand-lg navbar-dark">
-			<a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>">
-				<img src="<?php assets_url();?>brand/<?php echo get_theme_mod("sendeturm_active_theme", "styles"); ?>.svg" width="30" height="30" class="d-inline-block align-top mr-1" alt="">
-				<span class="name"><?php bloginfo('name');?></span>
-			</a>
-			
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
+		<header id="page-header">
+			<div id="main-nav" class="navbar navbar-expand-lg navbar-dark">
+				<a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>">
+					<img src="<?php assets_url();?>brand/<?php echo get_theme_mod("sendeturm_active_theme", "styles"); ?>.svg" width="30" height="30" class="d-inline-block align-top mr-1" alt="">
+					<span class="name"><?php bloginfo('name');?></span>
+				</a>
+				
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
 
-			<div class="collapse navbar-collapse" id="navbarNav">
-				<?php bootstrap_navigation('header-menu', 'navbar-nav ml-auto mr-2 mt-2 mt-lg-0'); ?>
+				<div class="collapse navbar-collapse" id="navbarNav">
+					<?php bootstrap_navigation('header-menu', 'navbar-nav ml-auto mr-2 mt-2 mt-lg-0'); ?>
 
-				<form id="header-search" class="form-inline" action="<?php echo home_url( '/' ); ?>" method="get">
-					<input class="form-control" name="s" value="<?php the_search_query(); ?>" type="search" placeholder="<?php _e('Search', 'sendeturm'); ?>" aria-label="<?php _e('Search', 'sendeturm'); ?>">
-					<button class="btn" type="submit">
-					<svg width="15px" height="15px">
-						<path d="M11.618 9.897l4.224 4.212c.092.09.1.23.02.312l-1.464 1.46c-.08.08-.222.072-.314-.02L9.868 11.66M6.486 10.9c-2.42 0-4.38-1.955-4.38-4.367 0-2.413 1.96-4.37 4.38-4.37s4.38 1.957 4.38 4.37c0 2.412-1.96 4.368-4.38 4.368m0-10.834C2.904.066 0 2.96 0 6.533 0 10.105 2.904 13 6.486 13s6.487-2.895 6.487-6.467c0-3.572-2.905-6.467-6.487-6.467 "></path>
-					</svg>
-					</button>
-				</form>
+					<form id="header-search" class="form-inline" action="<?php echo home_url( '/' ); ?>" method="get">
+						<input class="form-control" name="s" value="<?php the_search_query(); ?>" type="search" placeholder="<?php _e('Search', 'sendeturm'); ?>" aria-label="<?php _e('Search', 'sendeturm'); ?>">
+						<button class="btn" type="submit">
+						<svg width="15px" height="15px">
+							<path d="M11.618 9.897l4.224 4.212c.092.09.1.23.02.312l-1.464 1.46c-.08.08-.222.072-.314-.02L9.868 11.66M6.486 10.9c-2.42 0-4.38-1.955-4.38-4.367 0-2.413 1.96-4.37 4.38-4.37s4.38 1.957 4.38 4.37c0 2.412-1.96 4.368-4.38 4.368m0-10.834C2.904.066 0 2.96 0 6.533 0 10.105 2.904 13 6.486 13s6.487-2.895 6.487-6.467c0-3.572-2.905-6.467-6.487-6.467 "></path>
+						</svg>
+						</button>
+					</form>
+				</div>
 			</div>
-		</div>
-	</header>
+		</header>
 
-	<div id="content" class="site-content">
+		<div id="content" class="site-content">
